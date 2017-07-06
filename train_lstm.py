5cc011cf3482c8321ed53c5ade23d234ac58a0b1
Pinae <jonny@pinae.net>
2017-07-06 16:39:03 +0200

    Added dropout

--- train_lstm.py
+++ train_lstm.py
@@ -5,6 +5,7 @@ from keras.datasets import reuters
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.utils.np_utils import to_categorical
@@ -20,6 +21,9 @@ def my_config():
     n_top_words = 5000
     max_text_length = 500
     embedding_vecor_dimensionality = 16
+    recurrent_dropout_factor = 0.2
+    LSTM_dropout_factor = 0.2
+    layer_dropout_factor = 0.0
 
 
 @ex.capture
@@ -39,13 +43,16 @@ def get_data(n_top_words, max_text_length, seed):
 
 
 @ex.automain
-def train_network(n_top_words, max_text_length, embedding_vecor_dimensionality):
+def train_network(n_top_words, max_text_length, embedding_vecor_dimensionality,
+                  recurrent_dropout_factor, LSTM_dropout_factor, layer_dropout_factor):
     X_train, y_train, X_test, y_test = get_data()
     print(X_train.shape)
     print(X_train[0])
     model = Sequential()
     model.add(Embedding(n_top_words, embedding_vecor_dimensionality, input_length=max_text_length))
-    model.add(LSTM(100))
+    model.add(Dropout(layer_dropout_factor))
+    model.add(LSTM(100, recurrent_dropout=recurrent_dropout_factor, dropout=LSTM_dropout_factor))
+    model.add(Dropout(layer_dropout_factor))
     model.add(Dense(46, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
