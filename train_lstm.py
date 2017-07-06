32474d97bb734af5e3f9c20550671800aff11804
Pinae <jonny@pinae.net>
2017-07-06 10:31:59 +0200

    Use Sacred

--- train_lstm.py
+++ train_lstm.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
-import numpy as np
 from keras.datasets import reuters
 from keras.models import Sequential
 from keras.layers import Dense
@@ -9,31 +8,48 @@ from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.utils.np_utils import to_categorical
+from sacred import Experiment
+from sacred.observers import MongoObserver
 
-n_top_words = 5000
-max_text_length = 500
-embedding_vecor_dimensionality = 32
-random_seed = 42
-np.random.seed(random_seed)
-(X_train, y_train), (X_test, y_test) = reuters.load_data(path="reuters.npz",
-                                                         num_words=n_top_words,
-                                                         skip_top=0,
-                                                         maxlen=max_text_length,
-                                                         test_split=0.2,
-                                                         seed=random_seed)
-word_index = reuters.get_word_index(path="reuters_word_index.json")
-X_train = sequence.pad_sequences(X_train, maxlen=max_text_length)
-X_test = sequence.pad_sequences(X_test, maxlen=max_text_length)
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+ex = Experiment('LSTM_Classification')
+ex.observers.append(MongoObserver.create())
 
-model = Sequential()
-model.add(Embedding(n_top_words, embedding_vecor_dimensionality, input_length=max_text_length))
-model.add(LSTM(100))
-model.add(Dense(46, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=64)
 
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+@ex.config
+def my_config():
+    n_top_words = 5000
+    max_text_length = 500
+    embedding_vecor_dimensionality = 16
+
+
+@ex.capture
+def get_data(n_top_words, max_text_length, seed):
+    (X_train, y_train), (X_test, y_test) = reuters.load_data(path="reuters.npz",
+                                                             num_words=n_top_words,
+                                                             skip_top=0,
+                                                             maxlen=max_text_length,
+                                                             test_split=0.2,
+                                                             seed=seed)
+    # word_index = reuters.get_word_index(path="reuters_word_index.json")
+    X_train = sequence.pad_sequences(X_train, maxlen=max_text_length)
+    X_test = sequence.pad_sequences(X_test, maxlen=max_text_length)
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    return X_train, y_train, X_test, y_test
+
+
+@ex.automain
+def train_network(n_top_words, max_text_length, embedding_vecor_dimensionality):
+    X_train, y_train, X_test, y_test = get_data()
+    print(X_train.shape)
+    print(X_train[0])
+    model = Sequential()
+    model.add(Embedding(n_top_words, embedding_vecor_dimensionality, input_length=max_text_length))
+    model.add(LSTM(100))
+    model.add(Dense(46, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=64)
+
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("Accuracy: %.2f%%" % (scores[1]*100))
