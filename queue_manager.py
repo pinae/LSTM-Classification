f01e10f2bac71079a90794ea46fb52b22404b09b
Pinae <jonny@pinae.net>
2017-07-28 10:41:37 +0200

    Show and clear queued experiments

--- queue_manager.py
+++ queue_manager.py
@@ -3,7 +3,7 @@
 from __future__ import division, print_function, unicode_literals
 from pymongo import MongoClient
 from time import sleep
-from train_lstm import ex
+import argparse
 
 client = MongoClient()
 db = client.sacred
@@ -11,6 +11,7 @@ running_experiments = []
 
 
 def start_experiment(config):
+    from train_lstm import ex
     run = ex.run(config_updates=config)
     try:
         db_entry = db.runs.find({'config': run.config})[0]
@@ -39,7 +40,38 @@ def check_for_work():
     start_experiment(config)
 
 
-if __name__ == "__main__":
+def main_loop():
     while True:
         check_for_work()
         sleep(10)
+
+
+def print_dict(d, indentation=2):
+    for key, value in sorted(d.items()):
+        if type(value) == dict:
+            print(" "*indentation + key + ":")
+            print_dict(value, indentation=indentation+2)
+        else:
+            print(" "*indentation + key + ": " + str(value))
+
+
+def list_experiments(status='QUEUED'):
+    print("These Experiments have the status '" + status + "':")
+    for ex in db.runs.find({'status': status}):
+        print("Experiment No " + str(ex['_id']))
+        print_dict(ex['config'], indentation=2)
+        print("----------------------------------------")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Manage queued Sacred experiments.\n" +
+                                                 "If called without parameters the queue_manager will fetch " +
+                                                 "experiments from the database and run them.")
+    parser.add_argument('-l', '--list', action='store_true', help="Show the list of queued experiments.")
+    parser.add_argument('-c', '--clear', action='store_true', help="Clear the list of queued experiments.")
+    args = parser.parse_args()
+    if args.clear:
+        db.runs.delete_many({'status': 'QUEUED'})
+    if args.list:
+        list_experiments()
+    elif not args.clear:
+        main_loop()
